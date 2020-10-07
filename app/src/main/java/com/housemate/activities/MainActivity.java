fbc9aa525fab591c1069aad4dae4ee2000939ad6
Adam746 <gi8320@wayne.edu>
2020-10-07 18:43:14 -0400

    Integrated User class, login, and register with DataSender

--- app/src/main/java/com/housemate/activities/MainActivity.java
+++ app/src/main/java/com/housemate/activities/MainActivity.java
@@ -2,8 +2,10 @@ package com.housemate.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.housemate.classes.Task;
+import com.housemate.classes.User;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //onRegister(null);
+        //onLogin(null);
     }
+
+    // Function to execute user authentication
+    public void onLogin (View view) {
+        User user = new User("username", "password");
+        user.login();
+
+    }
+
+    // Execute new user insert to database
+    public void onRegister (View view) {
+        User user = new User("username", "password", "email", "firstname", "lastname", 1, 1);
+        user.register();
+    }
+
 }
