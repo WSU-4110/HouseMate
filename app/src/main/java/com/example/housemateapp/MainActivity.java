1ade6d7de9a388a27a2ff2b276b4dabb66e4ce56
Fatima Hashmi <eu6179@wayne.edu>
2020-10-09 18:30:16 -0400

    preparing for merge

--- app/src/main/java/com/example/housemateapp/MainActivity.java
+++ app/src/main/java/com/example/housemateapp/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.housemateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+    private Button Register;
+    private TextView Info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.Username);
+        Password = (EditText)findViewById(R.id.Password);
+        Login = (Button)findViewById(R.id.Login);
+        Register = (Button)findViewById(R.id.Register);
+        Info = (TextView)findViewById(R.id.Info);
+
+    }
+
+    private void validate (String userName, String userPassword){
+        if ((userName == "Admin") && (userPassword == "1234")){
+
+        }
+
     }
 }
