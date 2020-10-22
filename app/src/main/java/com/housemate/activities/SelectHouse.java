c64875cddb16af43549318511a465da1c31f7d54
Adam746 <gi8320@wayne.edu>
2020-10-21 20:57:21 -0400

    Added Join household and Create household buttons to SelectHouse

--- app/src/main/java/com/housemate/activities/SelectHouse.java
+++ app/src/main/java/com/housemate/activities/SelectHouse.java
@@ -26,24 +26,67 @@ public class SelectHouse extends AppCompatActivity {
         }
     };
 
+    private View.OnClickListener joinBtnClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            goToJoin();
+        }
+    };
+
+    private View.OnClickListener createBtnClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            goToCreate();
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_house);
+
         ArrayList<Integer> houseList =  MainActivity.currentUser.getHouseId();
+        numHouses = houseList.size();
+
         ll = (LinearLayout) findViewById(R.id.selectHouse);
         lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        numHouses = houseList.size();
+        Button b;
         for (int i = 0; i < numHouses; i++) {
-            Button b = new Button(this);
+            b = new Button(this);
+
             b.setId(houseList.get(i));
             b.setText(Household.getHouseholdInfo(houseList.get(i)));
             b.setOnClickListener(btnClicked);
+
             ll.addView(b,lp);
         }
 
+        b = new Button(this);
+        b.setText("Join Household");
+        b.setOnClickListener(joinBtnClicked);
+
+        ll.addView(b,lp);
+
+        b = new Button(this);
+        b.setText("Create New Household");
+        b.setOnClickListener(createBtnClicked);
+
+        ll.addView(b,lp);
+
     }
+
+    private void goToJoin() {
+        Intent intent = new Intent(this, JoinHousehold.class);
+        startActivity(intent);
+    }
+
+
+    private void goToCreate() {
+        Intent intent = new Intent(this, CreateHousehold.class);
+        startActivity(intent);
+    }
+
 
     private void buttonClicked(View v) {
         int id = v.getId();
