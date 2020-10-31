6862725b0c8c531336425a08129f8134715430a3
Blake Konkolesky <gp4406@wayne.edu>
2020-10-31 17:30:22 -0400

    Added edit and delete options to tasks in a household list and added an alternating color scheme

--- app/src/main/java/com/housemate/classes/TaskListAdapter.java
+++ app/src/main/java/com/housemate/classes/TaskListAdapter.java
@@ -1,18 +1,23 @@
 package com.housemate.classes;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.core.text.HtmlCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.housemate.activities.CreateTaskActivity;
 import com.housemate.activities.R;
 
 import java.util.List;
+
+import static androidx.core.content.ContextCompat.startActivity;
 
 public class TaskListAdapter extends RecyclerView.Adapter<TaskListAdapter.TaskListViewHolder> {
     private Context context;
@@ -21,11 +26,15 @@ public class TaskListAdapter extends RecyclerView.Adapter<TaskListAdapter.TaskLi
     public static class TaskListViewHolder extends RecyclerView.ViewHolder {
         public TextView taskView;
         public TextView assignedUserView;
+        public ImageView editTaskView;
+        public ImageView deleteTaskView;
 
         public TaskListViewHolder(@NonNull View view) {
             super(view);
             taskView = view.findViewById(R.id.task_text);
             assignedUserView = view.findViewById(R.id.assigned_user);
+            editTaskView = view.findViewById(R.id.edit_task_view);
+            deleteTaskView = view.findViewById(R.id.delete_task_view);
         }
     }
 
@@ -50,9 +59,16 @@ public class TaskListAdapter extends RecyclerView.Adapter<TaskListAdapter.TaskLi
                                     "<p>Due %s at %s</p>\n",
                                     task.getName(), task.getDescription(), task.getDueDate(), task.getDueTime());
         holder.taskView.setText(HtmlCompat.fromHtml(taskText, 0));
+        holder.taskView.setBackgroundColor((position % 2 == 0) ? 0xFFDDDDDD : 0xFFEEEEEE);
+
         String userText = String.format("<i>%s</i>", task.getAssignedUser());
         holder.assignedUserView.setText(HtmlCompat.fromHtml(userText, 0));
-        //holder.taskView.setBackgroundColor((position % 2 == 0) ? 0xFFDDDDDD : 0xFFEEEEEE);
+/*
+        holder.editTaskView.setOnClickListener(view -> context.startActivity(
+                new Intent(context, EditTaskActivity.class)));
+        holder.deleteTaskView.setOnClickListener(view -> context.startActivity(
+                new Intent(context, DeleteTaskActivity.class)));
+ */
     }
 
     @Override
