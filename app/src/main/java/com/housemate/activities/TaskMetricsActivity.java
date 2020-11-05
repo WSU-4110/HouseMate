6f6e5f7c6c9369bbce01aea4775a331186f66b80
Adam746 <gi8320@wayne.edu>
2020-11-05 15:50:00 -0500

    Create task metrics page

---
+++ app/src/main/java/com/housemate/activities/TaskMetricsActivity.java
@@ -0,0 +1,14 @@
+package com.housemate.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskMetricsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_metrics);
+    }
+}
