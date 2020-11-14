5d6e717c8ec8d2bebfa535ca2bc2233e5531c58b
Adam746 <gi8320@wayne.edu>
2020-11-14 15:28:45 -0500

    created Forgot Password Page

---
+++ app/src/main/java/com/housemate/activities/ForgotPasswordActivity.java
@@ -0,0 +1,20 @@
+package com.housemate.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+
+    public void gotoMainActivity(View view) {
+    }
+
+    public void requestPasswordReset(View view) {
+    }
+}
