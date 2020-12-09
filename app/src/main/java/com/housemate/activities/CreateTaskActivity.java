d7fec7ca4049442543b46f6f0ece111d10b8e32e
Cossins42 <fo8861@wayne.edu>
2020-12-08 23:45:19 -0500

    test

--- app/src/main/java/com/housemate/activities/CreateTaskActivity.java
+++ app/src/main/java/com/housemate/activities/CreateTaskActivity.java
@@ -2,13 +2,18 @@ package com.housemate.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.DatePickerDialog;
+import android.app.NotificationChannel;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -38,6 +43,8 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import androidx.core.app.NotificationCompat;
+import android.app.NotificationManager;
 
 //extends FragmentActivity
 //                          implements NoticeDialogFragment.NoticeDialogListener
@@ -57,6 +64,9 @@ public class CreateTaskActivity extends AppCompatActivity implements DatePickerD
     LocalDate dueDate;
     LocalTime dueTime;
     HousemateRecViewAdapter housemateRecViewAdapter;
+
+    NotificationChannel PushNotificationChannel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,6 +98,8 @@ public class CreateTaskActivity extends AppCompatActivity implements DatePickerD
 
 
 
+
+
         repeatTaskSpinner = findViewById(R.id.repeatTaskSpinner);
         ArrayAdapter<CharSequence> repeatTaskAdapter = ArrayAdapter.createFromResource(
                 this, R.array.repeat_task_arr, android.R.layout.simple_spinner_item);
@@ -110,9 +122,14 @@ public class CreateTaskActivity extends AppCompatActivity implements DatePickerD
                 task.create(MainActivity.currentHousehold.getHouseID());
 
 
+
                 Intent intent = new Intent(CreateTaskActivity.this, HomePageActivity.class);
                 startActivity(intent);
+
+
+
             }
+
         });
         cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -238,5 +255,19 @@ public class CreateTaskActivity extends AppCompatActivity implements DatePickerD
         }
     }
 
-
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("jk", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 }
